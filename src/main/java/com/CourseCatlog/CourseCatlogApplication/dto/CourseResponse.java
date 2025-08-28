bdbf9699854ec3ddb5766e2326b97e11799d33b7
DHINA <dhinagarandhinagar70@gmail.com>
2025-08-28 09:54:53 +0530

    Course API Changes

--- src/main/java/com/CourseCatlog/CourseCatlogApplication/dto/CourseResponse.java
+++ src/main/java/com/CourseCatlog/CourseCatlogApplication/dto/CourseResponse.java
@@ -1,18 +1,33 @@
 package com.CourseCatlog.CourseCatlogApplication.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
-import lombok.Data;
+//import jakarta.validation.constraints.NotBlank;
+//import lombok.Builder;
+//import lombok.Data;
+//
+//import java.time.Instant;
+//
+//@Data @Builder
+//public class CourseResponse {
+//    private Long id;
+//    private String title;
+//    private String description;
+//    private String category;
+//    private String instructor;
+//    private String language;
+//    private Instant createdAt;
+//}
 
+
+import lombok.Builder;
 import java.time.Instant;
 
-@Data @Builder
-public class CourseResponse {
-    private Long id;
-    private String title;
-    private String description;
-    private String category;
-    private String instructor;
-    private String language;
-    private Instant createdAt;
-}
+@Builder
+public record CourseResponse(
+        Long id,
+        String title,
+        String description,
+        String category,
+        String instructor,
+        String language,
+        Instant createdAt
+) {}
--- src/main/java/com/CourseCatlog/CourseCatlogApplication/dto/UpdateCourseRequest.java
+++ src/main/java/com/CourseCatlog/CourseCatlogApplication/dto/UpdateCourseRequest.java
@@ -1,10 +1,12 @@
 package com.CourseCatlog.CourseCatlogApplication.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpdateCourseRequest {
     private String title;
+    @Size(max = 2000, message = "Description cannot exceed 2000 characters")
     private String description;
     private String category;
     private String instructor;
