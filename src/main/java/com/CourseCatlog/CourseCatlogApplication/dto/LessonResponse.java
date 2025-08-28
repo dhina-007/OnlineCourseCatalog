cbc7d0ce56fcea11e5f91a012e295a3b5c603e69
DHINA <dhinagarandhinagar70@gmail.com>
2025-08-28 11:03:24 +0530

    Lessons API

--- src/main/java/com/CourseCatlog/CourseCatlogApplication/dto/LessonResponse.java
+++ src/main/java/com/CourseCatlog/CourseCatlogApplication/dto/LessonResponse.java
@@ -1,19 +1,12 @@
 package com.CourseCatlog.CourseCatlogApplication.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
-import lombok.Data;
-
 import java.util.List;
 
-@Data @Builder
-public class LessonResponse {
-    private Long id;
-    private String title;
-    private String content;
-    private String duration;
-    private String videoUrl;
-    private List<MaterialResponse> resources;
-    //private List<ResourceDto> resources;
-
-}
+public record LessonResponse(
+        Long id,
+        String title,
+        String content,
+        String duration,
+        String videoUrl,
+        List<MaterialResponse> resources
+) {}
