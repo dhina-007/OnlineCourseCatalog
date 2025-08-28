8f5e46e01bcadb5c1ddfe7ff4b3508a8bd87fe14
dhina <152854225+dhina-007@users.noreply.github.com>
2025-08-28 10:18:36 +0530

    Update CourseResponse.java

--- src/main/java/com/CourseCatlog/CourseCatlogApplication/dto/CourseResponse.java
+++ src/main/java/com/CourseCatlog/CourseCatlogApplication/dto/CourseResponse.java
@@ -1,23 +1,5 @@
 package com.CourseCatlog.CourseCatlogApplication.dto;
 
-//import jakarta.validation.constraints.NotBlank;
-//import lombok.Builder;
-//import lombok.Data;
-//
-//import java.time.Instant;
-//
-//@Data @Builder
-//public class CourseResponse {
-//    private Long id;
-//    private String title;
-//    private String description;
-//    private String category;
-//    private String instructor;
-//    private String language;
-//    private Instant createdAt;
-//}
-
-
 import lombok.Builder;
 import java.time.Instant;
 
